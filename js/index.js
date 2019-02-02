900f7ec27e872ca66990c057c94bf5eb30edba12
Memitaru <amandatwah@gmail.com>
2019-02-01 19:39:54 -0800

    Added on-click to buttons

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+let button = document.querySelectorAll('.btn');
+
+button.forEach(function(btn){
+    btn.addEventListener('click', (event) => { event.target.style.backgroundColor = 'darkblue'; });
+})
+
